2760a43bde87e039d4689873990ef5041cf78170
Steve Bendiola <steve.bendiola@gmail.com>
2011-01-28 11:30:08 -0600

    added another example

--- src/main/scala/dynamic.scala
+++ src/main/scala/dynamic.scala
@@ -1,23 +1,30 @@
 import scala.util.parsing.json.JSON
 
-class JSONList(items: Iterable[Any]) extends Dynamic {
+class ListAccessor(items: Iterable[Any]) extends Dynamic {
   def invokeDynamic(name: String)(args: Any*) = {
     val value = items.collect { case l:Iterable[_] =>  l.collect { case (n, v) if (n == name) => v } }.flatten.headOption
-    println("name: ", name, items, "result: ", value)
+    println(Map("name" -> name, "args" -> args, "items" -> items, "result" -> value))
     new JSONList(value)
   }
   override def typed[T] = items.headOption.map(_.asInstanceOf[T]).get
   override def toString = "JSONList(" + items.toString + ")"
 }
 
-object JSONList {
+object JsonListAccessor {
   def apply(text: String): JSONList = 
-    new JSONList(JSON.parse(text))     
+    new ListAccessor(JSON.parse(text))     
 }
 
-val json = JSONList("""{ "foo" : { "bar" : { "baz" : 23  }   }, "cuz" : "abc" }""")
+val json = JsonListAccessor("""{ "foo" : { "bar" : { "baz" : 23  }   }, "cuz" : "abc" }""")
 val number = json.foo.bar.baz.typed[Double]
 val text = json.cuz.typed[String]
 require(number == 23d)
 println(text, text.getClass)
 require(text == "abc")
+val items = List(List(("foo",List(("bar",List(("baz",23.0))))), ("cuz","abc")))
+items.foreach(println)
+val listsAndMaps = new ListAccessor(items)
+require(listsAndMaps.foo.bar.baz.typed[Double] == 23)
+require(listsAndMaps.foo.bar.typed[List[Any]] == List("baz" -> 23))
+require(listsAndMaps.cuz.typed[String] == "abc")
+
